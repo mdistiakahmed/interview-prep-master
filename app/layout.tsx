6be42f475b47a5bb6cd6f4821421afdea7f4e551
Md Istiak Ahmed <istinishat@gmail.com>
2024-11-06 13:42:58 +0600

    Add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,6 +20,9 @@ export const metadata: Metadata = {
   title: "Interview Preperation for software engineers",
   description:
     "InterViewPrepMaster offers top-notch resources and guides to help you ace every interview. From software engineering and system design to problem-solving and behavioral interviews, get ready to succeed with our expert tips and in-depth preparation materials. Join our community today and take the first step towards landing your dream job.",
+  other: {
+    "google-site-verification": "suDh686UnWZz7NkSxQb23W2SV2IU5F2jAm8McRxwHCc",
+  },
   openGraph: {
     title: "Interview Preperation for software engineers",
     description:
@@ -45,6 +49,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-BP7EGJ1CED" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
