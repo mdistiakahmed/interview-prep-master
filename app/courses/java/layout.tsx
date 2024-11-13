e525e29894152adc4dc29aab49632c896b68c5c0
Md Istiak Ahmed <istinishat@gmail.com>
2024-11-13 06:37:39 +0600

    Update laout file for java course

--- app/courses/java/layout.tsx
+++ app/courses/java/layout.tsx
@@ -1,4 +1,28 @@
 import SystemDesignSidebar from "@/components/course-sidebar/CourseSidebar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Java Interview Preparation",
+  description:
+    "Dive into essential Java concepts and skills to help you succeed in your interview.",
+  openGraph: {
+    title: "Java Interview Preparation",
+    description:
+      "Dive into essential Java concepts and skills to help you succeed in your interview.",
+    type: "website",
+    locale: "en_US",
+    url: `https://www.interviewprepmaster.com/courses/java`,
+    siteName: "InterviewPrepMaster",
+    images: [
+      {
+        url: "/java.png",
+        width: 1200,
+        height: 630,
+        alt: "java logo",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
--- app/courses/java/page.tsx
+++ app/courses/java/page.tsx
@@ -1,48 +1,62 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Page = () => {
   const lessons = [
     {
-      title: "Object-Oriented Programming (OOP)",
-      description:
-        "Understand the principles of OOP in Java, including inheritance, polymorphism, encapsulation, and abstraction.",
+      title: "Basic Syntax",
+      description: "Get familiar with basic java syntax",
       image: "/java.png",
+      path: `/courses/java/java-syntax-basics`,
     },
     {
       title: "Java Collections Framework",
       description:
-        "Explore Java's Collections Framework, covering data structures like ArrayList, HashMap, and LinkedList, essential for handling data in Java.",
+        "Use collection framework cheatsheet to recap the available methods and operations",
       image: "/java.png",
+      path: `/courses/java/collection-framework-arraylist`,
+    },
+    {
+      title: "Additional Section",
+      description:
+        "Get some additional knowledge before you sit on the interview",
+      image: "/java.png",
+      path: `/courses/java/additional-strings`,
     },
     {
       title: "Exception Handling",
       description:
         "Learn about Java's robust exception handling, focusing on try-catch blocks, checked and unchecked exceptions, and custom exceptions.",
       image: "/java.png",
+      path: `/courses/java`,
     },
     {
       title: "Multithreading and Concurrency",
       description:
         "Dive into Java multithreading concepts, covering thread lifecycle, synchronization, and the Executor framework for concurrent programming.",
       image: "/java.png",
+      path: `/courses/java`,
     },
     {
       title: "Java Streams API",
       description:
         "Master the Java Streams API for functional-style operations on collections, including filter, map, reduce, and parallel processing.",
       image: "/java.png",
+      path: `/courses/java`,
     },
     {
       title: "Memory Management & Garbage Collection",
       description:
         "Understand Java’s memory management, garbage collection, and performance tuning for efficient resource use in applications.",
       image: "/java.png",
+      path: `/courses/java`,
     },
     {
       title: "JVM Architecture",
       description:
         "Learn the architecture of the Java Virtual Machine (JVM), including the class loader, runtime data areas, and the execution engine.",
       image: "/java.png",
+      path: `/courses/java`,
     },
   ];
 
@@ -58,24 +72,27 @@ const Page = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {lessons.map((lesson, index) => (
-          <div
+          <Link
+            href={lesson.path}
             key={index}
             className="bg-white p-4 border hover:shadow-xl transition-shadow duration-200 cursor-pointer"
           >
-            <Image
-              src={lesson.image}
-              alt={`${lesson.title} graphic`}
-              height={200}
-              width={200}
-              className="w-full h-32 object-cover "
-            />
             <div className="mt-4">
-              <h2 className="text-xl font-semibold text-gray-800">
+              <h2 className="text-xl font-semibold text-gray-800 text-center">
                 {lesson.title}
               </h2>
-              <p className="text-gray-600 mt-2">{lesson.description}</p>
+              <Image
+                src={lesson.image}
+                alt={`${lesson.title} graphic`}
+                height={200}
+                width={200}
+                className="w-full h-32 object-cover "
+              />
+              <p className="text-gray-600 mt-2 text-center">
+                {lesson.description}
+              </p>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
